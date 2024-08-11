6992312e8b72440ac4100060532d4bc6500582cb
Fabbaist <weberfabian1@gmx.de>
2024-08-11 22:13:35 +0200

    add double space checker to input checks

--- rushT2/input.c
+++ rushT2/input.c
@@ -15,17 +15,40 @@ int *allocate_views()
     return views;
 }
 
+int validate_input_sequence(char *input_string)
+{
+    int last_was_space = 0;
+
+    while (*input_string)
+    {
+        if (*input_string >= '1' && *input_string <= '4')
+            last_was_space = 0;
+        else if (*input_string == ' ')
+            last_was_space = 1;
+        else
+        {
+            if (last_was_space)
+            {
+                putstr("Error");
+                return 0;
+            }
+            putstr("Error");
+            return 0;
+        }
+        input_string++;
+    }
+    return 1;
+}
+
 int parse_input(char *input_string, int *views)
 {
     int k = 0;
+
     while (*input_string && k < 16)
     {
         if (*input_string >= '1' && *input_string <= '4')
-            views[k++] = *input_string - '0';
-        else if (*input_string != ' ')
         {
-            putstr("Error");
-            return 0;
+            views[k++] = *input_string - '0';
         }
         input_string++;
     }
@@ -45,6 +68,9 @@ int validate_input(int k, int *views)
 
 int *input(char *input_string)
 {
+    if (!validate_input_sequence(input_string))
+        return 0;
+
     int *views = allocate_views();
     if (!views)
         return 0;
