6b43d7f02c1d58ca6b47eb0a51ffd0506cf3125a
Fabbaist <weberfabian1@gmx.de>
2024-08-11 21:25:01 +0200

    Validate all views function split to comply with 25 lines max

--- rushT2/validate.c
+++ rushT2/validate.c
@@ -34,37 +34,45 @@ int is_valid(int **numbers_array, int row, int col, int num, int *views)
     return 1;
 }
 
-int validate_solution(int **numbers_array, int *views)
+int check_all_views(int *row, int *col, int *views, int i)
 {
-    int i;
+    if (!check_view(row, views[12 + i])) // Right view
+        return 0;
+    if (!check_view(col, views[i])) // Up view
+        return 0;
+
+    for (int j = 0; j < 4; j++)
+        col[j] = row[3 - j];
+
+    if (!check_view(col, views[4 + i])) // Down view
+        return 0;
+
+    return 1;
+}
+
+int validate_row_col(int **numbers_array, int *views, int i)
+{
     int temp_row[4];
     int temp_col[4];
 
-    // Check all rows
-    while (i < 4)
+    for (int j = 0; j < 4; j++)
     {
-        if (!check_view(numbers_array[i], views[8 + i])) // Left view
+        temp_row[j] = numbers_array[i][3 - j];
+        temp_col[j] = numbers_array[j][i];
+    }
+
+    if (!check_view(numbers_array[i], views[8 + i])) // Left view
+        return 0;
+
+    return check_all_views(temp_row, temp_col, views, i);
+}
+
+int validate_solution(int **numbers_array, int *views)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (!validate_row_col(numbers_array, views, i))
             return 0;
-        int j = 0;
-        while (j < 4)
-        {
-            temp_row[j] = numbers_array[i][3 - j];
-            temp_col[j] = numbers_array[j][i];
-            j++;
-        }
-        if (!check_view(temp_row, views[12 + i])) // Right view
-            return 0;
-        if (!check_view(temp_col, views[i])) // Up view
-            return 0;
-        j = 0;
-        while (j < 4)
-        {
-            temp_col[j] = numbers_array[3 - j][i];
-            j++;
-        }
-        if (!check_view(temp_col, views[4 + i])) // Down view
-            return 0;
-        i++;
     }
     return 1;
 }
